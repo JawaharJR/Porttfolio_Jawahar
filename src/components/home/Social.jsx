fd5f72a3d686d90910d7cf5cd6096d50cf93b398
JawaharJR <107339319+JawaharJR@users.noreply.github.com>
2023-04-01 15:30:21 +0530

    desktop

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div className="home__social">
+
+      <a
+        href="https://in.linkedin.com/in/jawaharjr"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/JawaharJR"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  );
+}
+
+export default Social;
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,45 @@
+import Work1 from "../../assets/Nationalizeapi.png";
+import work2 from '../../assets/crm.png'
+import work3 from '../../assets/Student portal.png'
+import Work4 from "../../assets/URL Shortner.png"
+
+
+export const projectsData = [
+    {
+        id:1,
+        image: Work1,
+        title: 'Nationalize API',
+        category: "Full Stack Web",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB",
+        demoLink: "https://github.com/JawaharJR/NationalizeAPI.git",
+        sourceCode:"https://glittering-kheer-3d1407.netlify.app/"
+    },
+    {
+        id:2,
+        image: work2,
+        title: 'CRM App',
+        category: "Full Stack Web",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB",
+        demoLink: "https://delicate-concha-4a3e4b.netlify.app/",
+        sourceCode:"http://github.com/JawaharJR/webcode2_crm_app"
+    },
+    {
+        id:3,
+        image: work3,
+        title: 'Student Task Submission Portal',
+        category: "Full Stack Web",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB",
+        demoLink: "https://dancing-ganache-7d43da.netlify.app/",
+        sourceCode:"https://github.com/JawaharJR/Student-Task-Submission-Portal_frontend.git"
+    },
+
+    {
+        id:4,
+        image: Work4,
+        title: 'URL Shortner',
+        category: "Full Stack App",
+        techStack: "HTML, CSS, ReactJS, NodeJS, MongoDB",
+        demoLink: "https://meek-tiramisu-b137bf.netlify.app/",
+        sourceCode:"https://github.com/JawaharJR/url-shortner-frontend.git"
+    },
+]
